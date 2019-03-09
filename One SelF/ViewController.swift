e0c7abddc9ec07c4aa7b0424d2e39740ec4e15fc
玉木涼祐 <ryosuketamaki@tamakiryouyuunomacbook-puro.local>
2019-03-09 21:12:57 +0900

    変更後。

--- One SelF/ViewController.swift
+++ One SelF/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+    @IBOutlet weak var label: UIButton!
+    override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
 }
-
+    @IBAction func handleCreateAccountButton(_ sender: Any) {
+        if let address = mailAddressTextField.text, let password = passwordTextField.text, let displayName = displayNameTextField.text {
+            
+            // アドレスとパスワードと表示名のいずれかでも入力されていない時は何もしない
+            if address.isEmpty || password.isEmpty || displayName.isEmpty {
+                print("DEBUG_PRINT: 何かが空文字です。")
+                return
+            }
+            
+            // アドレスとパスワードでユーザー作成。ユーザー作成に成功すると、自動的にログインする
+            Auth.auth().createUser(withEmail: address, password: password) { user, error in
+                if let error = error {
+                    // エラーがあったら原因をprintして、returnすることで以降の処理を実行せずに処理を終了する
+                    print("DEBUG_PRINT: " + error.localizedDescription)
+                    return
+                }
+                print("DEBUG_PRINT: ユーザー作成に成功しました。")
+                
+                // 表示名を設定する
+                let user = Auth.auth().currentUser
+                if let user = user {
+                    let changeRequest = user.createProfileChangeRequest()
+                    changeRequest.displayName = displayName
+                    changeRequest.commitChanges { error in
+                        if let error = error {
+                            // プロフィールの更新でエラーが発生
+                            print("DEBUG_PRINT: " + error.localizedDescription)
+                            return
+                        }
+                        print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました。")
+                        
+                        // 画面を閉じてViewControllerに戻る
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            }
+        }
+ }
