3b8acb43354f95bf141cf6787111217cd182e6af
玉木涼祐 <ryosuketamaki@tamakiryouyuunomacbook-puro.local>
2019-03-09 19:10:34 +0900

    Initial Commit

---
+++ One SelF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  One SelF
+//
+//  Created by 玉木涼祐 on 2019/03/09.
+//  Copyright © 2019年 ryosuke tamaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
